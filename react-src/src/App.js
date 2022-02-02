e32f9ef02b36687f9403301f282c3b8c6459a4d3
Pranav Murali <pranavmurali2001@gmail.com>
2022-02-03 00:26:44 +0530

    :bookmark: Navbar init

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {React} from 'react';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Header from './components/Header/Header.jsx';
+import Login from "./components/Login/Login.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-black z-0">
+      <Header/> 
+          <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/login' element={<Login />}/>
+          </Routes>
+        </div>
+    </Router>
   );
 }
-
 export default App;
